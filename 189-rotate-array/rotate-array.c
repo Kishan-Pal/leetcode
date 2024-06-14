45f2720b99a9fa1b28040fd83e9b29e25fdec0d4
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-06-15 00:12:03 +0530

    Time: 100 ms (35.34%) | Memory: 21.9 MB (54.64%) - LeetSync

--- 189-rotate-array/rotate-array.c
+++ 189-rotate-array/rotate-array.c
@@ -12,14 +12,6 @@ void reverse(int *a, int l, int r) {
 void rotate(int* nums, int numsSize, int k) {
     k = k % numsSize;
     int temp;
-    if(k == numsSize/2.0){
-        for(int i=0; i<k; i++){
-            temp = nums[i];
-            nums[i] = nums[i+k];
-            nums[i+k] = temp;
-        }
-        return;
-    }
     reverse(nums, 0, numsSize-1);
     reverse(nums, 0, k-1);
     reverse(nums, k, numsSize-1);
