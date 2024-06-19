b8ccb7c3ae1d8180bc56cd3ae5431ec5a4f695e4
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-06-19 20:53:18 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (35.61%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,32 +1,23 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int n=digits.length, sum=0, i;
+        int n=digits.length, carry=0, i;
+        int[] digits2 = new int[n+1];
         digits[n-1] = digits[n-1] + 1;
-        for(i=n-1; i>=0; i--){
-            sum = sum + digits[i];
-            if(sum <= 9){
-                digits[i] = sum;
-                sum = 0;
-                break;
-            }
-            else {
-                digits[i] = sum-10;
-                sum = 1;
-            }
-        }
-        System.out.println(sum +" "+ digits[0]);
-        if(sum == 0){
+        if(digits[n-1] <= 9)
             return digits;
-        }
-        else{
-            int[] digits2= new int[n+1];
-            digits2[0] = 1;
-            for(i=0; i<n; i++) {
-                digits2[i+1] = digits[i];
+        carry = 1;
+        digits[n-1] = digits[n-1] - 10;
+        for(i=n-2; i>=0; i--) {
+            digits[i] = digits[i] + carry;
+            if(digits[i] <= 9) {
+                return digits;
             }
-            return digits2;
+            digits2[i+2] = 0;
+            digits[i] = digits[i] - 10;
+            carry = 1;
         }
-
-        
+        digits2[1] = 0;
+        digits2[0] = 1;
+        return digits2;
     }
 }
