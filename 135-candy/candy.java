a206e49b125e0b1e2c059bd683fe5810feabcd18
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-06-20 23:55:59 +0530

    Time: 265 ms (5.69%) | Memory: 45.4 MB (47.17%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -2,12 +2,13 @@ class Solution {
     public int candy(int[] ratings) {
         int n=ratings.length;
         if(n == 1) return 1;
-        int i, candy, pre;
+        int i, sum = 1;
         
         int a[] = new int[n], j=0,temp;
         a[0] = 1;
         while(j<n-1 && ratings[j] > ratings[j+1]) {
             a[0] = a[0] + 1;
+            sum = sum + 1;
             j = j+1;
         }
         for(i=1; i<n-1; i++) {
@@ -27,22 +28,14 @@ class Solution {
                     j = j+1;
                 }
                 a[i] = Math.max(a[i-1] + 1, temp);
-                System.out.println(" max = " + a[i] + " a[i-1] = " + a[i-1]);
             }
+            sum = sum + a[i];
         }
-        System.out.println("a[n-1] " + a[n-1]);
         if(ratings[n-2] < ratings[n-1]) {
-            a[n-1] = a[n-2] + 1;
+            sum = sum + a[n-2] + 1;
         }
         else{
-            a[n-1] = 1;
-        }
-        for(int k = 0; k<n; k++) {
-            System.out.println(a[k]);
-        }
-        int sum = 0;
-        for(int l=0; l<n; l++) {
-            sum = sum + a[l];
+            sum = sum + 1;
         }
         return sum;
     }
