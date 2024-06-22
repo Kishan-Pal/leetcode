5e9652a2da13b7832ebed8eafcdaa9d89a9cf80a
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-06-22 18:26:19 +0530

    Time: 7 ms (100.00%) | Memory: 49 MB (77.34%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -1,21 +1,17 @@
 class NumArray {
     int[] nums;
-    ArrayList<Integer> sum;
     public NumArray(int[] nums) {
-        this.nums = nums;
-        this.sum = new ArrayList<>();
-        int n = nums.length, temp = 0;
-        for(int i=0; i<n; i++){
-            temp = temp + nums[i];
-            sum.add(temp);
+        for(int i=1; i<nums.length; i++) {
+            nums[i] = nums[i] + nums[i-1];
         }
+        this.nums = nums;
     }
     //-2, -2, 1, -4, -2, -3
     public int sumRange(int left, int right) {
         if(left == 0) 
-            return this.sum.get(right);
+            return this.nums[right];
         else
-            return this.sum.get(right) - this.sum.get(left-1);
+            return this.nums[right] - this.nums[left-1];
     }
 }
 
