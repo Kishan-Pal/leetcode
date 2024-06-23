f427d28f0caef04552f911f794dd36dd872adf45
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-06-24 00:51:14 +0530

    Time: 1 ms (100.00%) | Memory: 45.4 MB (59.02%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
@@ -1,16 +1,43 @@
 class Solution {
     public int maximumProduct(int[] nums) {
+        // Arrays.sort(nums);
+        // return Math.max(nums[nums.length-1]*nums[nums.length-2]*nums[nums.length-3],nums[nums.length-1]*nums[0]*nums[1]);
         int n = nums.length;
-        int max = Integer.MIN_VALUE, temp;
-        Arrays.sort(nums);
-        temp = nums[n-1] * nums[n-2] * nums[n-3];
-        max = (max > temp? max: temp);
-        temp = nums[0] * nums[n-1] * nums[n-2];
-        max = (max > temp? max: temp);
-        temp = nums[1] * nums[0] * nums[n-1];
-        max = (max > temp? max: temp);
-        temp = nums[2] * nums[1] * nums[0];
-        max = (max > temp? max: temp);
-        return max;
+        if(n == 3) 
+            return nums[0] * nums[1] * nums[2];        
+
+        int fMax = Integer.MIN_VALUE;
+        int sMax = Integer.MIN_VALUE;
+        int tMax = Integer.MIN_VALUE;
+        int fMin = Integer.MAX_VALUE;
+        int sMin = Integer.MAX_VALUE;
+        for(int i : nums){
+            if(i > tMax){
+                if(i > sMax){
+                    if(i > fMax){
+                        tMax = sMax;
+                        sMax = fMax;
+                        fMax = i;
+                    }
+                    else{
+                        tMax = sMax;
+                        sMax = i;
+                    }
+                }
+                else{
+                    tMax = i;
+                }
+            }
+            if(i < sMin){
+                if(i < fMin){
+                    sMin = fMin;
+                    fMin = i;
+                }
+                else{
+                    sMin = i;
+                }
+            }
+        }
+        return Math.max(fMin * sMin * fMax, fMax * sMax * tMax);
     }
 }
