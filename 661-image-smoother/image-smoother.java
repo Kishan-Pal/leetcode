516105a29cf443f03e4a4e439728b5adf5cc3894
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-06-24 22:15:34 +0530

    Time: 3 ms (99.17%) | Memory: 45.2 MB (91.70%) - LeetSync

---
+++ 661-image-smoother/image-smoother.java
@@ -0,0 +1,50 @@
+class Solution {
+    public void smooth3by3(int[][] img, int[][] res, int i, int j) {
+        int count = 0;
+        int sum = 0;
+        if(i>0 && j>0) {
+            sum = sum + img[i-1][j-1];
+            count = count + 1;
+        }
+        if(i>0){
+            sum = sum + img[i-1][j];
+            count = count + 1;
+        }
+        if(i>0 && j<img[0].length-1){
+            sum = sum + img[i-1][j+1];
+            count = count + 1;
+        }
+        if(i<img.length-1 && j>0) {
+            sum = sum + img[i+1][j-1];
+            count = count + 1;
+        }
+        if(i<img.length-1){
+            sum = sum + img[i+1][j];
+            count = count + 1;
+        }
+        if(i<img.length-1 && j<img[0].length-1){
+            sum = sum + img[i+1][j+1];
+            count = count + 1;
+        }
+        if(j>0){
+            sum = sum + img[i][j-1];
+            count = count + 1;
+        }
+        if(j<img[0].length-1){
+            sum = sum + img[i][j+1];
+            count = count + 1;
+        }
+        sum = sum + img[i][j];
+        count = count + 1;
+        res[i][j] = sum/count;
+    }
+    public int[][] imageSmoother(int[][] img) {
+        int[][] res = new int[img.length][img[0].length];
+        for(int i=0; i<img.length; i++) {
+            for(int j=0; j<img[0].length; j++) {
+                this.smooth3by3(img, res, i, j);
+            }
+        }
+        return res;
+    }
+}
