2c80dd7331f4d78e8dadec9a7870b8b2aec1ab91
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-06-24 22:30:19 +0530

    Time: 7 ms (45.37%) | Memory: 45.5 MB (52.70%) - LeetSync

--- 661-image-smoother/image-smoother.java
+++ 661-image-smoother/image-smoother.java
@@ -1,56 +1,59 @@
 class Solution {
-    public void smooth3by3(int[][] img, int[][] res, int i, int j, int m, int n) {
-        int count = 0;
-        int sum = 0;
-        boolean ig = i>0;
-        boolean il = i<m-1;
-        boolean jg = j>0;
-        boolean jl = j<n-1;
-        if(ig){
-            sum = sum + img[i-1][j];
-            count = count + 1;
-            if(jg) {
-                sum = sum + img[i-1][j-1];
-                count = count + 1;
-            }
-            if(jl){
-                sum = sum + img[i-1][j+1];
-                count = count + 1;
-            }
-        }
-        
-        
-        if(il){
-            sum = sum + img[i+1][j];
-            count = count + 1;
-            if(jg) {
-                sum = sum + img[i+1][j-1];
-                count = count + 1;
-            }
-            if(jl){
-                sum = sum + img[i+1][j+1];
-                count = count + 1;
-            }
-        }
-        
-        if(jg){
-            sum = sum + img[i][j-1];
-            count = count + 1;
-        }
-        if(jl){
-            sum = sum + img[i][j+1];
-            count = count + 1;
-        }
-        sum = sum + img[i][j];
-        count = count + 1;
-        res[i][j] = sum/count;
-    }
     public int[][] imageSmoother(int[][] img) {
         int m = img.length, n = img[0].length;
+        int count = 0;
+        int sum = 0;
+        boolean ig;
+        boolean il;
+        boolean jg;
+        boolean jl;
         int[][] res = new int[m][n];
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
-                this.smooth3by3(img, res, i, j, m, n);
+                count = 0;
+                sum = 0;
+                ig = i>0;
+                il = i<m-1;
+                jg = j>0;
+                jl = j<n-1;
+                if(ig){
+                    sum = sum + img[i-1][j];
+                    count = count + 1;
+                    if(jg) {
+                        sum = sum + img[i-1][j-1];
+                        count = count + 1;
+                    }
+                    if(jl){
+                        sum = sum + img[i-1][j+1];
+                        count = count + 1;
+                    }
+                }
+                
+                
+                if(il){
+                    sum = sum + img[i+1][j];
+                    count = count + 1;
+                    if(jg) {
+                        sum = sum + img[i+1][j-1];
+                        count = count + 1;
+                    }
+                    if(jl){
+                        sum = sum + img[i+1][j+1];
+                        count = count + 1;
+                    }
+                }
+                
+                if(jg){
+                    sum = sum + img[i][j-1];
+                    count = count + 1;
+                }
+                if(jl){
+                    sum = sum + img[i][j+1];
+                    count = count + 1;
+                }
+                sum = sum + img[i][j];
+                count = count + 1;
+                res[i][j] = sum/count;
             }
         }
         return res;
