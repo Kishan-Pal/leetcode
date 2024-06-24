23895b074f619f2bd6e0a9db6977ec2dd8841510
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-06-24 22:22:55 +0530

    Time: 3 ms (99.17%) | Memory: 45.3 MB (84.65%) - LeetSync

--- 661-image-smoother/image-smoother.java
+++ 661-image-smoother/image-smoother.java
@@ -2,38 +2,42 @@ class Solution {
     public void smooth3by3(int[][] img, int[][] res, int i, int j) {
         int count = 0;
         int sum = 0;
-        if(i>0){
+        boolean ig = i>0;
+        boolean il = i<img.length-1;
+        boolean jg = j>0;
+        boolean jl = j<img[0].length-1;
+        if(ig){
             sum = sum + img[i-1][j];
             count = count + 1;
-            if(j>0) {
+            if(jg) {
                 sum = sum + img[i-1][j-1];
                 count = count + 1;
             }
-            if(j<img[0].length-1){
+            if(jl){
                 sum = sum + img[i-1][j+1];
                 count = count + 1;
             }
         }
         
         
-        if(i<img.length-1){
+        if(il){
             sum = sum + img[i+1][j];
             count = count + 1;
-            if(j>0) {
+            if(jg) {
                 sum = sum + img[i+1][j-1];
                 count = count + 1;
             }
-            if(j<img[0].length-1){
+            if(jl){
                 sum = sum + img[i+1][j+1];
                 count = count + 1;
             }
         }
         
-        if(j>0){
+        if(jg){
             sum = sum + img[i][j-1];
             count = count + 1;
         }
-        if(j<img[0].length-1){
+        if(jl){
             sum = sum + img[i][j+1];
             count = count + 1;
         }
