2a3ba303e1f170bca52e392c55183cad0b8b5018
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-06-24 23:59:18 +0530

    Time: 3 ms (44.99%) | Memory: 41.4 MB (81.11%) - LeetSync

--- 682-baseball-game/baseball-game.java
+++ 682-baseball-game/baseball-game.java
@@ -5,29 +5,24 @@ class Solution {
         for(String i: operations) {
             switch(i) {
                 case "D":
-                    temp = res.get(k-1) * 2;
-                    res.add(temp);
-                    sum += temp;
+                    res.add(res.get(k-1) * 2);
                     k = k + 1;
                     break;
                 case "C":
                     k = k - 1;
-                    temp = res.get(k);
-                    res.remove(k);
-                    sum -= temp;
+                    res.remove(res.get(k));
                     break;
                 case "+":
-                    temp = res.get(k-1) + res.get(k-2);
-                    res.add(temp);
-                    sum += temp;
+                    res.add(res.get(k-1) + res.get(k-2));
                     k = k + 1;
                     break;
                 default:
-                    temp = Integer.parseInt(i);
-                    res.add(temp);
-                    sum += temp;
+                    res.add(Integer.parseInt(i));
                     k = k + 1;
             }
+        }
+        for(Integer i: res) {
+            sum += i;
         }
         return sum;
     }
