f71c063031d89fa298939f1d271d0a83efa9ff79
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-06-26 01:00:30 +0530

    Time: 1 ms (100.00%) | Memory: 42.7 MB (84.99%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -5,16 +5,13 @@ class Solution {
         char a, b;
         while(i<j) {
             a = s.charAt(i);
-            //System.out.println("a " + a);
             if((int)a <= 90 && (int)a >= 65)
                 a = (char)((int)a + 32);
             if((int)a <= 122 && (int)a >= 97 || (int)a <= 57 && (int)a >= 48){
                 b = s.charAt(j);
-                //System.out.println("b " + b);
                 if((int)b <= 90 && (int)b >= 65)
                     b = (char)((int)b + 32);
                 if((int)b <= 122 && (int)b >= 97 || (int)b <= 57 && (int)b >= 48){
-                    //System.out.println(a + " == " + b);
                     if(a != b) return false;
                     i = i + 1;
                     j = j - 1;
