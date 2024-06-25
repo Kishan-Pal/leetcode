e5ba2b8fc49ed519469e9927ab6ce394b05ebc61
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-06-25 21:14:48 +0530

    Time: 7 ms (11.12%) | Memory: 44.5 MB (8.37%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -4,7 +4,6 @@ class Solution {
         int i=n-1, j=m-1;
         String res = "";
         while(i >= 0 && j >= 0) {
-            System.out.println((a.charAt(i) - '0') + " " + (b.charAt(j) - '0'));
             sum = (a.charAt(i) - '0') + (b.charAt(j) - '0') + carry;
             res = (sum % 2) + res;
             carry = sum / 2;
@@ -26,7 +25,6 @@ class Solution {
         if(carry == 1) {
             res = 1 + res;
         }
-        System.out.println(carry);
         return res;
     }
 }
