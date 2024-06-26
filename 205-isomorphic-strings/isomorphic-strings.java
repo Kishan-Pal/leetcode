1466667d58e5176930ca82fa658dbf5ebd7e332a
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-06-27 00:40:26 +0530

    Time: 17 ms (24.05%) | Memory: 42.4 MB (43.45%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -6,10 +6,16 @@ class Solution {
         int j=0, k=0, temp1, temp2;
         for(int i=0; i<n; i++) {
             char c1 = s.charAt(i);
-            hs.put(c1, hs.getOrDefault(c1, j++));
+            if(!hs.containsKey(c1)) {
+                hs.put(c1, j);
+                j = j + 1;
+            }
             temp1 = hs.get(c1);
             char c2 = t.charAt(i);
-            ht.put(c2, ht.getOrDefault(c2, k++));
+            if(!ht.containsKey(c2)){
+                ht.put(c2, k);
+                k = k + 1;
+            }
             temp2 = ht.get(c2);
             //System.out.println(is[i] + " " + it[i]);
             if(temp1 != temp2) return false;
