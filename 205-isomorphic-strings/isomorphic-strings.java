8427ce9e846b1eb0f6d0941ccda96ae10c062bd1
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-06-27 00:51:26 +0530

    Time: 5 ms (96.41%) | Memory: 42.9 MB (10.23%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -5,9 +5,15 @@ class Solution {
         int j=1, k=1, temp1, temp2;
         for(int i=0; i<s.length(); i++) {
             char c1 = s.charAt(i);
-            temp1 = hs[(int)c1] = (hs[(int)c1] == 0? j++: hs[(int)c1]);
+            if(hs[(int)c1] == 0) {
+                hs[(int)c1] = j++;
+            }
+            temp1 = hs[(int)c1];
             char c2 = t.charAt(i);
-            temp2 = ht[(int)c2] = (ht[(int)c2] == 0? k++: ht[(int)c2]);
+            if(ht[(int)c2] == 0) {
+                ht[(int)c2] = k++;
+            }
+            temp2 = ht[(int)c2];
             //System.out.println(is[i] + " " + it[i]);
             if(temp1 != temp2) return false;
         }
