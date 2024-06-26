2d2a8aa78ab21c693614a8d7d551ca218bce4fa1
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-06-27 00:36:26 +0530

    Time: 15 ms (31.71%) | Memory: 42.5 MB (21.55%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -3,24 +3,22 @@ class Solution {
         HashMap<Character, Integer> hs = new HashMap<>();
         HashMap<Character, Integer> ht = new HashMap<>();
         int n = s.length();
-        int j=0, k=0;
-        int[] is = new int[n];
-        int[] it = new int[n];
+        int j=0, k=0, temp1, temp2;
         for(int i=0; i<n; i++) {
             char c1 = s.charAt(i);
             if(!hs.containsKey(c1)) {
                 hs.put(c1, j);
                 j = j + 1;
             }
-            is[i] = hs.get(c1);
+            temp1 = hs.get(c1);
             char c2 = t.charAt(i);
             if(!ht.containsKey(c2)){
                 ht.put(c2, k);
                 k = k + 1;
             }
-            it[i] = ht.get(c2);
+            temp2 = ht.get(c2);
             //System.out.println(is[i] + " " + it[i]);
-            if(is[i] != it[i]) return false;
+            if(temp1 != temp2) return false;
         }
         return true;
     }
