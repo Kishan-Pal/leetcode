677321eb8021d2a9ab1e6947fcfbf01758bb4891
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-06-27 00:03:39 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (54.49%) - LeetSync

--- 168-excel-sheet-column-title/excel-sheet-column-title.java
+++ 168-excel-sheet-column-title/excel-sheet-column-title.java
@@ -7,13 +7,11 @@ class Solution {
             temp = columnNumber % 26;
             if(temp == 0) {
                 res = "Z" + res;
-                //System.out.println(26 + " " + a[25]);
                 columnNumber -= 26;
                 
             }
             else {
-                res = a[temp-1] + res;
-                //System.out.println(temp + " " + a[temp-1]);
+                res = a[temp-1] + res;  
             }
             columnNumber /= 26;
             
