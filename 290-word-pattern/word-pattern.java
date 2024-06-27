da0b0a6fad145bd0c3503ad5f00b4abe50416ef9
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-06-27 18:34:01 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (83.17%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        HashMap<String, Character> hash = new HashMap<>();
+        int[] a = new int[200];
+        int st, ed=-1, i;
+        String temp = "";
+        for(i=0; i<pattern.length() && ed < s.length(); i++) {
+            st = ed = ed + 1;
+            while(ed < s.length() && s.charAt(ed) != ' ') ed++;
+            temp = s.substring(st, ed);
+            //System.out.println(temp);
+            if(a[(int)pattern.charAt(i)] > 0 && hash.containsKey(temp)){
+                if(pattern.charAt(i) != hash.get(temp)) return false;
+            }
+            else if(a[(int)pattern.charAt(i)] == 0 && !hash.containsKey(temp)){
+                a[(int)pattern.charAt(i)] = 1;
+                hash.put(temp, pattern.charAt(i));
+            }
+            else return false;
+        }
+        //System.out.println(s.length() + " " + ed);
+        if(s.length() != ed || i < pattern.length()) return false;
+        return true;
+    }
+}
