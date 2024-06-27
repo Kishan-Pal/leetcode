d9f1782f484e1a32c08d77c8221486eb049b51af
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-06-27 20:26:43 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (75.19%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int[] count = new int[123];
         char[] news = s.toCharArray();
         int j=0, k;
         for(int i=0; i<news.length; i++) {
