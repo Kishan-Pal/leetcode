7c41bb70d764de2592bcc854c681badac9eceb10
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-07-11 22:00:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (64.20%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -5,7 +5,8 @@ class Solution {
             j--;
         }
         //System.out.println(j);
-        Arrays.sort(nums, j, nums.length);
+        //Arrays.sort(nums, j, nums.length);
+        this.reverse(nums, j, nums.length-1);
         if(j==0) return;
         int num = nums[j-1];
         int k = j;
@@ -13,5 +14,15 @@ class Solution {
         
         nums[j-1] = nums[k];
         nums[k] = num;
+    }
+    public void reverse(int[] nums, int left, int right) {
+        while(left < right) {
+            nums[left] = nums[left] ^ nums[right];
+            nums[right] = nums[left] ^ nums[right];
+            nums[left] = nums[left] ^ nums[right];
+            left++;
+            right--;
+
+        }
     }
 }
