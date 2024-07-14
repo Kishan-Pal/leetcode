865b244682ade43295b9a9ebfe8a8d6bb41ccb83
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-07-14 23:21:39 +0530

    Time: 21 ms (6.68%) | Memory: 44.6 MB (5.22%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -0,0 +1,48 @@
+class Solution {
+    int[] count;
+    HashSet<List<Integer>> res;
+    List<Integer> tempList;
+    int sum, target;
+
+    Solution() {
+        this.count = new int[51];
+        this.res = new HashSet<>();
+        this.tempList = new ArrayList<>();
+        this.sum = 0;
+    }
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        this.target = target;
+        for(int i: candidates) {
+            this.count[i]++;
+        }
+        for(int i: this.count) {
+            System.out.println(i);
+        }
+        this.solve(30);
+        return new ArrayList<>(this.res);
+    }
+
+    public void solve(int i) {
+        while(i >= 0 && this.count[i] == 0) i--;
+        if(i <= 0) return;
+        int k=count[i];
+        for(int j=0; j<count[i]; j++) {
+            this.sum += i;
+            this.tempList.add(i);
+        }
+        while(k > 0) {
+            if(this.sum == this.target) {
+                this.res.add(new ArrayList<>(tempList));
+            }
+            else if(this.sum < this.target){
+                solve(i-1);
+            }
+            this.sum -= i;
+            this.tempList.removeLast();
+            k--;
+        }
+        solve(i-1);
+    }
+
+}
