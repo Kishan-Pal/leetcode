d44a00808419a38586340129dcabe8a4dadbb31f
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-09-30 19:40:55 +0530

    Time: 87 ms (97.73%) | Memory: 41.4 MB (78.98%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,25 +1,25 @@
 class Solution {
 
     public boolean search(char[][] board, int i, int j, String word, int k, boolean[][] visited) {
-        boolean result;
+        //boolean result;
         //System.out.println(i + " " + j + " == " + k);
         visited[i][j] = true;
         if(k == word.length()-1) return true;
         if(i>0 && !visited[i-1][j] && board[i-1][j] == word.charAt(k+1)) {
-            result = search(board, i-1, j, word, k+1, visited);
-            if(result) return true;
+            if(search(board, i-1, j, word, k+1, visited)) return true;
+            //if(result) return true;
         }
         if(j<board[0].length-1 && !visited[i][j+1] && board[i][j+1] == word.charAt(k+1)) {
-            result = search(board, i, j+1, word, k+1, visited);
-            if(result) return true;
+            if(search(board, i, j+1, word, k+1, visited)) return true;
+            //if(result) return true;
         }
         if(i<board.length-1 && !visited[i+1][j] && board[i+1][j] == word.charAt(k+1)) {
-            result = search(board, i+1, j, word, k+1, visited);
-            if(result) return true;
+            if(search(board, i+1, j, word, k+1, visited)) return true;
+            //if(result) return true;
         }
         if(j>0 && !visited[i][j-1] && board[i][j-1] == word.charAt(k+1)) {
-            result = search(board, i, j-1, word, k+1, visited);
-            if(result) return true;
+            if(search(board, i, j-1, word, k+1, visited)) return true;
+            //if(result) return true;
         }
         visited[i][j] = false;
         return false;
