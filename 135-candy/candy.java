9f6e8f509d93e8823b2a655003dff6ef4edbfe63
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-10-02 23:41:15 +0530

    Time: 3 ms (80.52%) | Memory: 45.4 MB (71.35%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,41 +1,23 @@
 class Solution {
     public int candy(int[] ratings) {
-        int n=ratings.length;
-        if(n == 1) return 1;
-        int i, sum = 1;
-        
-        int a[] = new int[n], j=0,temp;
-        a[0] = 1;
-        while(j<n-1 && ratings[j] > ratings[j+1]) {
-            a[0] = a[0] + 1;
-            sum = sum + 1;
-            j = j+1;
-        }
-        for(i=1; i<n-1; i++) {
-            a[i] = 1;
-            if(ratings[i-1] >= ratings[i]) {
-                j=i;
-                while(j<n-1 && ratings[j] > ratings[j+1]) {
-                    a[i] = a[i] + 1;
-                    j = j+1;
-                }
+        if(ratings.length == 1) return 1;
+        int[] candies = new int[ratings.length];
+        int sum = 0;
+        Arrays.fill(candies, 1);
+
+        for(int i=1; i<ratings.length; i++) {
+            if(ratings[i] > ratings[i-1]) {
+                candies[i] = candies[i-1] + 1;
             }
-            else {
-                temp = 1;
-                j=i;
-                while(j<n-1 && ratings[j] > ratings[j+1]) {
-                    temp = temp + 1;
-                    j = j+1;
-                }
-                a[i] = Math.max(a[i-1] + 1, temp);
+
+        }
+        for(int i=ratings.length-2; i>=0; i--) {
+            if(ratings[i] > ratings[i+1] && candies[i] <= candies[i+1]) {
+                candies[i] = candies[i+1] + 1;
             }
-            sum = sum + a[i];
         }
-        if(ratings[n-2] < ratings[n-1]) {
-            sum = sum + a[n-2] + 1;
-        }
-        else{
-            sum = sum + 1;
+        for(int i: candies) {
+            sum += i;
         }
         return sum;
     }
