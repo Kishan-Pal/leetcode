641be53e90d8471d78035df14e17573f0fb76195
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-10-03 00:16:52 +0530

    Time: 5 ms (99.18%) | Memory: 46.3 MB (86.22%) - LeetSync

--- 561-array-partition/array-partition.java
+++ 561-array-partition/array-partition.java
@@ -1,10 +1,33 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
-        int sum = 0;
-        Arrays.sort(nums);
-        for(int i=0; i<nums.length; i+=2) {
-            sum = sum + nums[i];
+        int[] count = new int[20002];
+        int var = 10000;
+        boolean flag = false;
+        int temp = 0, j, sum = 0;
+        for(int i: nums) {
+            count[i + var]++;
         }
+        // for(int i=0; i<20002; i++) {
+        //     if(count[i] >= 1) System.out.println(i + " " + count[i]);
+        // }
+
+        for(int i=0; i<=20001; i++) {
+            if(count[i] == 0) continue;
+            if(flag){
+                count[i]--;
+                sum += temp;
+            }
+            j = count[i] / 2;
+            sum += (i - var) * j;
+            if(count[i] % 2 == 1) {
+                flag = true;
+                temp = i - var;
+            }
+            else {
+                flag = false;
+            }
+        }
+
         return sum;
     }
 }
