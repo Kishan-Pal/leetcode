8a2bf0904e84f253f9f681e390be53ecbb6b3384
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-10-05 22:09:40 +0530

    Time: 11 ms (41.86%) | Memory: 58 MB (5.21%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -10,35 +10,48 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        ListNode cur = head, removed, pre, cur2;
-        int temp;
-        if(head == null) return null;
-        while(cur.next != null) {
-            if(cur.val > cur.next.val) {
-                removed = cur.next;
-                cur.next = cur.next.next;
-                pre = null;
-                cur2 = head;
-                while(cur2 != null && cur2 != cur && cur2.val < removed.val) {
-                    pre = cur2;
-                    cur2 = cur2.next;
-
-                }
-                if(pre == null) {
-                    removed.next = head;
-                    head = removed;
-                }
-                else {
-                    removed.next = pre.next;
-                    pre.next = removed;
-                }
-                
-            }
-            else {
-
+        return sort(head, null);
+    }
+    public ListNode sort(ListNode start, ListNode end) {
+        if(start == end || start == null || start.next == null) return start;
+        ListNode pre, cur, cur2;
+        cur = start;
+        cur2 = start;
+        pre = null;
+        while(cur2 != null && cur2.next != null) {
+            pre = cur;
+            cur = cur.next;
+            cur2 = cur2.next.next;
+        }
+        pre.next = null;
+        ListNode head1 = sort(start, pre);
+        ListNode head2 = sort(cur, cur2);
+        ListNode head = new ListNode(10101010);
+        cur = head1;
+        cur2 = head2;
+        pre = head;
+        while(cur != null && cur2 != null) {
+            if(cur.val < cur2.val) {
+                pre.next = cur;
                 cur = cur.next;
             }
+            else {
+                pre.next = cur2;
+                cur2 = cur2.next;
+
+            }
+            pre = pre.next;
         }
-        return head;
+        while(cur != null) {
+            pre.next = cur;
+            cur = cur.next;
+            pre = pre.next;
+        }
+        while(cur2 != null) {
+            pre.next = cur2;
+            cur2 = cur2.next;
+            pre = pre.next;
+        }
+        return head.next;
     }
 }
