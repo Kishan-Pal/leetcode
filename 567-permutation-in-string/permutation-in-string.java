c30a731b7e6a1fcc653cc588b8fc6cf50b2fb07e
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-10-06 15:16:04 +0530

    Time: 195 ms (13.97%) | Memory: 45.2 MB (9.76%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -2,18 +2,19 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         int[] hash2 = new int[27];
         int[] hash;
-        int n = s1.length();
+        int n1 = s1.length(), n;
+        int n2 = s2.length();
         int j;
-        for(int i=0; i<s1.length(); i++) {
+        for(int i=0; i<n1; i++) {
             hash2[(int) (s1.charAt(i) - 'a')]++;
         }
-        for(int i=0; i<s2.length(); i++) {
+        for(int i=0; i<n2; i++) {
             hash = hash2.clone();
-            n = s1.length();
+            n = n1;
             if(--hash[(int) (s2.charAt(i) - 'a')] >= 0) {
                 n--;
                 j = i + 1;
-                while(n > 0 && j < s2.length()) {
+                while(n > 0 && j < n2) {
                     if(--hash[(int) (s2.charAt(j) - 'a')] >= 0) {
                         n--;
                     }
