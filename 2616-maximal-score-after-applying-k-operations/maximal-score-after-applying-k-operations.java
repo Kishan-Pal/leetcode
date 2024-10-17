1295fbe70a446ce08c2c747d33a580262cc3cecf
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-10-17 16:31:43 +0530

    Time: 167 ms (5.07%) | Memory: 63.8 MB (6.19%) - LeetSync

---
+++ 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.java
@@ -0,0 +1,28 @@
+class myComparator implements Comparator<Integer> {
+    public int compare(Integer a, Integer b) {
+        if (a > b) {
+            return -1;
+        } else if (a < b) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
+
+class Solution {
+    public long maxKelements(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new myComparator());
+        long result = 0;
+        for (int i : nums) {
+            pq.add(i);
+        }
+        System.out.println(pq);
+        for(int i=0; i<k; i++) {
+            int ele = pq.poll();
+            result += ele;
+            pq.add((ele + 2)/3);
+        }
+        return result;
+    }
+}
