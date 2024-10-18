f73a945f02bfb1df298b8d4e2a0f6bf3884faa60
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-10-18 19:56:55 +0530

    Time: 9 ms (67.71%) | Memory: 41.1 MB (55.85%) - LeetSync

---
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public void countRecursive(int[] nums, int maxOr, int curOr, int i, int[] count) {
+        if(i == nums.length) {
+            if(curOr == maxOr) {
+                count[0]++;
+            }
+            return;
+        }
+        //considering the current element
+        countRecursive(nums, maxOr, curOr|nums[i], i+1, count);
+        // ignoring the current elelemt
+        countRecursive(nums, maxOr, curOr, i+1, count);
+    }
+
+    public int countMaxOrSubsets(int[] nums) {
+        int maxOr = 0;
+        int[] count = new int[1];
+        count[0] = 0;
+        for(int i: nums) {
+            maxOr |= i;
+        }
+        //System.out.println("Max OR = " + maxOr);
+        countRecursive(nums, maxOr, 0, 0, count);
+        //System.out.println(count[0]);
+        return count[0];
+    }
+}
