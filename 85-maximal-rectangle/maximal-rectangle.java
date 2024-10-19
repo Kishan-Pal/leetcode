35f16f16bb68e0c3bbebbfb006decc2e2475b317
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2024-10-19 22:07:18 +0530

    Time: 11 ms (76.74%) | Memory: 47.1 MB (24.17%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.java
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -1,7 +1,6 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
         int[][] dp = new int[matrix.length][matrix[0].length];
-        int[][] dp2 = new int[matrix.length][matrix[0].length];
         int res = 0;
         for(int i=0; i<matrix.length; i++) {
             dp[i][matrix[0].length-1] = (int)(matrix[i][matrix[0].length-1] - '0');
@@ -36,34 +35,5 @@ class Solution {
         // }
         return res;
 
-        
-        // System.out.println();
-        // System.out.println(res);
-        // for(int j=matrix[0].length-1; j>=0; j--) {
-        //     for(int i=matrix.length-2; i>=0; i--) {
-        //         if(dp[i][j] != 0 && dp[i+1][j] != 0) {
-        //             dp[i][j] = Math.min(dp[i][j], dp[i+1][j]) * 2;
-        //         }
-                
-        //     }
-        // }
-        // for(int j=0; j<matrix[0].length; j++) {
-        //     dp2[matrix.length-1][j] = (int)(matrix[matrix.length-1][j] - '0');
-        // }
-        // for(int j=matrix[0].length-1; j>=0; j--) {
-        //     for(int i=matrix.length-2; i>=0; i--) {
-        //         //System.out.println((int)(matrix[i][j] - '0'));
-        //         if((int)(matrix[i][j] - '0') != 0) {
-        //             dp2[i][j] = (int)(matrix[i][j] - '0') + dp2[i+1][j];
-        //         }
-        //     }
-        // }
-
-        // for(int[] i: dp2) {
-        //     for(int j: i) {
-        //         System.out.print(j + " ");
-        //     }
-        //     System.out.println();
-        // }
     }
 }
