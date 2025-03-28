4dc93ebf28337213695b73fe9c65e79d7b8c92d9
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2025-03-28 15:25:21 +0530

    Time: 5 ms (33.91%) | Memory: 49.5 MB (70.50%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean[][] visited;
+    public int numIslands(char[][] grid) {
+        visited = new boolean[grid.length][grid[0].length];
+        int count = 0;
+        // dfs(grid, 0, 0);
+        // for(int l=0; l<grid.length; l++) {
+        //     for(int m=0; m<grid[0].length; m++) {
+        //         if(visited[l][m])
+        //             System.out.print(1 + " ");
+        //         else 
+        //             System.out.print(0 + " ");
+        //     }
+        //     System.out.println();
+        // }
+        for(int i=0; i<grid.length; i++) {
+            for(int j=0; j<grid[0].length; j++) {
+                if(dfs(grid, i, j)) {
+                    //System.out.println("one in " + i + ", " + j);
+                    count++;
+                    // for(int l=0; l<grid.length; l++) {
+                    //     for(int m=0; m<grid[0].length; m++) {
+                    //         if(visited[l][m])
+                    //             System.out.print(1 + " ");
+                    //         else 
+                    //             System.out.print(0 + " ");
+                    //     }
+                    //     System.out.println();
+                    // }
+                }
+            }
+        }
+        return count;
+    }
+    public boolean dfs(char[][] grid, int row, int col) {
+        if(row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || visited[row][col] || grid[row][col] == '0') return false;
+
+        visited[row][col] = true;
+        boolean result = true;
+    
+
+        result = dfs(grid, row, col - 1) || result;
+        result = dfs(grid, row, col + 1) || result;
+        result = dfs(grid, row - 1, col) || result;
+        result = dfs(grid, row + 1, col) || result;
+        return result;
+    }
+}
