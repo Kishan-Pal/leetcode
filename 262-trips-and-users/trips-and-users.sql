e864d35fc92db2d8159634c0894c9a6ea2230424
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2025-06-16 14:59:25 +0530

    Time: 527 ms (83.19%) | Memory: 0B (100.00%) - LeetSync

--- 262-trips-and-users/trips-and-users.sql
+++ 262-trips-and-users/trips-and-users.sql
@@ -1,15 +1,11 @@
-# Write your MySQL query statement below
-with valid_row as (
-    select t.id, t.status, t.request_at
-    from Trips t
-    join Users c on t.client_id = c.users_id
-    join Users d on t.driver_id = d.users_id
-    where request_at between '2013-10-01' and '2013-10-03' 
-    and c.banned = 'No' and d.banned = 'No'
-)
-
-select request_at as 'Day', round(sum(status!='completed')/count(status), 2) as 'Cancellation Rate'
-from valid_row
-group by request_at
-
--- select * from valid_row
+SELECT 
+    request_at AS Day, 
+    ROUND(
+        SUM(CASE WHEN status LIKE '%cancelled%' THEN 1 ELSE 0 END) / COUNT(1),2) AS "Cancellation Rate"
+FROM 
+    Trips
+WHERE
+    client_id NOT IN (SELECT users_id FROM Users WHERE role = 'client' AND banned = 'Yes') and 
+    driver_id not in (select users_id from Users where role = 'driver' and banned ='Yes') 
+    AND request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY 1 
