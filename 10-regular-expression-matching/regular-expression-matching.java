f583a7ff14fd0cf0858f64e4d58c5dc918f3fd5b
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2025-07-08 15:56:35 +0530

    Time: 1 ms (100.00%) | Memory: 42.2 MB (62.06%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,43 +1,34 @@
 class Solution {
+    enum Right {
+        TRUE, FALSE
+    }
+    public static Right[][] mapping;
     public boolean isMatch(String s, String p) {
-        if(p.length() == 0) {
-            if(s.length() == 0) {
-                return true;
-            }
-            else {
-                return false;
+        mapping = new Right[s.length()+1][p.length()+1];
+        return dp(0, 0, s, p);
+    }
+
+    public boolean dp(int i, int j, String s, String p) {
+        if(j == p.length()) {
+            return i == s.length();
+        }
+        if(mapping[i][j] != null) {
+            return mapping[i][j] == Right.TRUE;
+        }
+        boolean firstMatch = (i < s.length()) && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.');
+        boolean ans;
+        if(j < p.length() - 1) {
+            if(p.charAt(j+1) == '*') {
+                ans = dp(i, j+2, s, p) || firstMatch && dp(i+1, j, s, p);
+                mapping[i][j] = (ans? Right.TRUE: Right.FALSE);
+                return ans;
             }
         }
-
-        
-
-        if(p.length() == 1) {
-            if(s.length() == 0) return false;
-            if(p.charAt(0) == s.charAt(0) || p.charAt(0) == '.') {
-                return isMatch(s.substring(1), "") ;
-            }
-            return false;
+        if(j < p.length()) {
+            ans = firstMatch && dp(i+1, j+1, s, p);
+            mapping[i][j] = (ans? Right.TRUE: Right.FALSE);
+            return ans;
         }
-
-        if(p.charAt(1) == '*') {
-            if(s.length() == 0) {
-                return isMatch(s, p.substring(2));
-            }
-            if(p.charAt(0) == '.' || s.charAt(0) == p.charAt(0)) {
-                boolean endStar = isMatch(s, p.substring(2));
-                boolean continueStar = isMatch(s.substring(1), p);
-                return endStar || continueStar;
-            }
-            if(p.charAt(0) != s.charAt(0)) {
-                return isMatch(s, p.substring(2));
-            }
-            return false;
-        }
-        if(s.length() == 0) return false;
-        if(p.charAt(0) == '.' || p.charAt(0) == s.charAt(0)) {
-            return isMatch(s.substring(1), p.substring(1));
-        }
-
         return false;
     }
 }
