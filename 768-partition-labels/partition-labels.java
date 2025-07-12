fb83c4aeb980068b28ddc22c67a02958f8987a4d
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2025-07-12 21:26:55 +0530

    Time: 4 ms (93.77%) | Memory: 42.7 MB (28.34%) - LeetSync

--- 768-partition-labels/partition-labels.java
+++ 768-partition-labels/partition-labels.java
@@ -1,15 +1,16 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
         int[] lastocc = new int[200];
-        for(int i=0; i<s.length(); i++) {
+        int n = s.length();
+        for(int i=0; i<n; i++) {
             lastocc[(int)s.charAt(i)] = i;
         }
         List<Integer> res = new ArrayList<>();
         int begin = 0;
         int end = -1;
-        for(int i=0; i<s.length(); i++) {
+        for(int i=0; i<n; i++) {
             end = Math.max(end, lastocc[(int)s.charAt(i)]);
-            if(i == end || i == s.length()-1 || end == s.length() - 1) {
+            if(i == end || i == n-1 || end == n - 1) {
                 res.add(end-begin+1);
                 begin = end+1;
                 i = end;
