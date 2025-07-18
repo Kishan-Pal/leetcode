5e2999f8bf866fbc92570fa91a91a9894456854b
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2025-07-18 20:33:45 +0530

    Time: 347 ms (46.99%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -1,12 +1,11 @@
 -- Write your PostgreSQL query statement below
 
-with red as (
+
+Select "name" from SalesPerson
+where sales_id not in (
+    select sales_id from Orders
+    where com_id in (
     select com_id from Company
     where name like 'RED'
-),
-person_red as (
-    select sales_id from Orders
-    where com_id in (select com_id from red)
 )
-Select "name" from SalesPerson
-where sales_id not in (select sales_id from person_red);
+);
