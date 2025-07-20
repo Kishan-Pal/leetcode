854e08bcc99c770c4454792531f662fd6e8f5229
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2025-07-20 19:43:24 +0530

    Time: 13 ms (26.39%) | Memory: 42.2 MB (62.18%) - LeetSync

--- 1203-print-in-order/print-in-order.java
+++ 1203-print-in-order/print-in-order.java
@@ -5,29 +5,37 @@ class Foo {
         this.isSecond = false;
     }
 
-    public synchronized void first(Runnable printFirst) throws InterruptedException {
-        
+    public void first(Runnable printFirst) throws InterruptedException {
+        synchronized(this) {
+            printFirst.run();
+            isFirst = true;
+            notifyAll();
+        }
         // printFirst.run() outputs "first". Do not change or remove this line.
-        printFirst.run();
-        isFirst = true;
-        notifyAll();
+        
     }
 
-    public synchronized void second(Runnable printSecond) throws InterruptedException {
-        while(!isFirst) {
-            wait();
+    public void second(Runnable printSecond) throws InterruptedException {
+        synchronized(this) {
+            while(!isFirst) {
+                wait();
+            }
+            // printSecond.run() outputs "second". Do not change or remove this line.
+            printSecond.run();
+            isSecond = true;
+            notifyAll();
         }
-        // printSecond.run() outputs "second". Do not change or remove this line.
-        printSecond.run();
-        isSecond = true;
-        notifyAll();
+        
     }
 
-    public synchronized void third(Runnable printThird) throws InterruptedException {
-        while(!isSecond) {
-            wait();
+    public void third(Runnable printThird) throws InterruptedException {
+        synchronized(this) {
+            while(!isSecond) {
+                wait();
+            }
+            // printThird.run() outputs "third". Do not change or remove this line.
+            printThird.run();
         }
-        // printThird.run() outputs "third". Do not change or remove this line.
-        printThird.run();
+        
     }
 }
