4faaaf777f3a722f42ee42125df3694d469fbe61
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2025-07-21 01:02:48 +0530

    Time: 1423 ms (17.73%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+with 
+buyerids as (
+    select buyer_id from Orders
+    where order_date between '2019-01-01' and '2019-12-31'
+)
+select u.user_id as buyer_id, u.join_date, coalesce(count(buyer_id),0) as orders_in_2019
+from Users u left join buyerids on u.user_id = buyerids.buyer_id
+group by u.user_id;
