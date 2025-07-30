0f7f69298898bab12f492fbad275a051c9da5129
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2025-07-30 19:02:36 +0530

    Time: 4 ms (100.00%) | Memory: 97.4 MB (47.44%) - LeetSync

--- 3390-minimum-rectangles-to-cover-points/minimum-rectangles-to-cover-points.java
+++ 3390-minimum-rectangles-to-cover-points/minimum-rectangles-to-cover-points.java
@@ -3,10 +3,10 @@ class Solution {
         Arrays.sort(points, (a, b) -> {
             return a[0] - b[0];
         });
-        for(int[] i: points) {
-            System.out.println(new StringBuilder().append(i[0]).append(" : ").append(i[1]).toString());
-            // System.out.println(i[0] + " : " + i[1]);
-        }
+        // for(int[] i: points) {
+        //     System.out.println(new StringBuilder().append(i[0]).append(" : ").append(i[1]).toString());
+        //     // System.out.println(i[0] + " : " + i[1]);
+        // }
         int count = 0;
         int num;
         int i=0;
