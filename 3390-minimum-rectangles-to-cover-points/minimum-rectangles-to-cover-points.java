5a1797985998c6949ff24367db8735060ca23d8b
Kishan Pal <165324309+Kishan-Pal@users.noreply.github.com>
2025-07-30 19:00:06 +0530

    Time: 5 ms (66.67%) | Memory: 97.4 MB (47.44%) - LeetSync

---
+++ 3390-minimum-rectangles-to-cover-points/minimum-rectangles-to-cover-points.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minRectanglesToCoverPoints(int[][] points, int w) {
+        Arrays.sort(points, (a, b) -> {
+            return a[0] - b[0];
+        });
+        // for(int[] i: points) {
+        //     System.out.println(i[0] + " : " + i[1]);
+        // }
+        int count = 0;
+        int num;
+        for(int i=0; i<points.length;) {
+            num = points[i][0];
+            count++;
+            while(i<points.length && points[i][0] - num <= w) i++;
+        }
+        return count;
+    }
+}
